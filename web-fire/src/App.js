5df11b53a175c87499f4717be51320e42cb94ac2
Odysseas Zafeiriou <odysseas@wearereasonablepeople.com>
2020-02-12 23:21:00 +0100

    wip

--- web-fire/src/App.js
+++ web-fire/src/App.js
@@ -3,7 +3,7 @@ import floorPlan from './floor-plan.png';
 import './App.css';
 import humans from './revitHumans';
 import styled from 'styled-components';
-// import {move} from './styles';
+import {move} from './styles';
 import {take} from 'ramda';
 
 const HumanDot = styled.circle`
@@ -46,23 +46,31 @@ const tops = [1000, 2000, 3000];
 const lefts = [1000, 2000, 3000];
 
 const App = () => {
-  const usePrevious = (value) => {
-    // The ref object is a generic container whose current property is mutable ...
-    // ... and can hold any value, similar to an instance property on a class
-    const ref = useRef();
-    
-    // Store current value in ref
-    useEffect(() => {
-      ref.current = value;
-    }, [value]); // Only re-run if value changes
-    
-    // Return previous value (happens before update in useEffect above)
-    return ref.current;
+  const [humanPositions, setHumanPosition] = React.useState(Array(humans.length).fill(0))
+  const [stepNumber, setStepNumber] = React.useState(0);
+  const goForward = () => {
+    const newPositions = Array(humans.length).fill(stepNumber + 1);
+    setStepNumber(stepNumber + 1);
+    setHumanPosition(newPositions);
   }
-  const [index, setIndex] = React.useState(0);
-  const prevIndex = usePrevious(index);
-  const prevSteps = Array(humans.length).fill(prevIndex || 0)
-  const steps = Array(humans.length).fill(index)
+  // const usePrevious = (value) => {
+  //   // The ref object is a generic container whose current property is mutable ...
+  //   // ... and can hold any value, similar to an instance property on a class
+  //   const ref = useRef();
+    
+  //   // Store current value in ref
+  //   useEffect(() => {
+  //     ref.current = value;
+  //   }, [value]); // Only re-run if value changes
+    
+  //   // Return previous value (happens before update in useEffect above)
+  //   return ref.current;
+  // }
+  // const [index, setIndex] = React.useState(0);
+  // const prevIndex = usePrevious(index);
+  // const prevSteps = Array(humans.length).fill(prevIndex || 0)
+  // const steps = Array(humans.length).fill(index)
+
   // const [steps, setSteps] = React.useState([0, 0, 0])
   // const humanInstances = humans.map(human =>
   //   new Human(human[index].x, human[index].y, human, index)
@@ -70,20 +78,21 @@ const App = () => {
   // React.useEffect(() => {
   //   setSteps([index, index, index])
   // }, [index])
+
   return (
     <div className="App">
       <img src={floorPlan} className="App-logo" alt="logo" />
       <div style={{position: 'absolute', top: 10, left: `calc(50vw - ${imageWidth/2}px)`, width: imageWidth, height: imageHeight}}>
         <svg style={{position: 'absolute', top: topPadding, left: leftPadding}} width={0.6388 * imageWidth} height={0.91 * imageHeight} viewBox={`0 0 42070 46285`} xmlns="http://www.w3.org/2000/svg">
-          {humans.map((h, i) => (
+          {take(3, humans).map((h, i) => (
             <>
               {/* {console.log(hs[prevSteps[i]])} */}
               <HumanDot
                 // x={[h[prevSteps[i]].x, h[steps[i]].x]}
                 // y={[h[prevSteps[i]].y, h[steps[i]].y]}
                 key={h.id}
-                cx={h[steps[i]].x}
-                cy={46285 - h[steps[i]].y}
+                cx={h[humanPositions[i]].x}
+                cy={46285 - h[humanPositions[i]].y}
                 r="200"
                 stroke={`rgb(${Math.random() * 255}, 0, 0)`}
                 fill={`rgb(${Math.random() * 255}, 0, 0)`}
@@ -92,9 +101,9 @@ const App = () => {
           ))}
         </svg>
         <div style={{position: 'absolute', top: 20, right: 0}}>
-          <h5>{`Step: ${index}`}</h5>
-          <button onClick={() => setIndex(Math.max(0, index - 1))}>Prev Step</button>
-          <button onClick={() => setIndex(index + 1)}>Next Step</button>
+          <h5>{`Step: ${stepNumber}`}</h5>
+          {/* <button onClick={() => setIndex(Math.max(0, index - 1))}>Prev Step</button> */}
+          <button onClick={goForward}>Next Step</button>
         </div>
       </div>
     </div>
