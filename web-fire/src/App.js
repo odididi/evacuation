d6f81d0fb9dcd92d4695fb4effa419aea9b0839c
Odysseas Zafeiriou <odysseas@wearereasonablepeople.com>
2020-02-23 00:49:59 +0100

    working me 300 collision

--- web-fire/src/App.js
+++ web-fire/src/App.js
@@ -1,7 +1,7 @@
 import React, {useRef, useEffect} from 'react';
-import floorPlan from './floor-plan.png';
+import floorPlan from './floor-plan.jpg';
 import './App.css';
-import humans from './revitHumans';
+import humans from './revitNew.json';
 import styled from 'styled-components';
 import {move} from './styles';
 import {take, reduce, addIndex} from 'ramda';
@@ -29,9 +29,9 @@ const reduceIndexed = addIndex(reduce);
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
 const imageHeight = screenHeight - 20;
-const imageWidth = (1.29 * screenHeight - 20);
-const leftPadding = 0.18065 * imageWidth;
-const topPadding = 0.045 * imageHeight;
+const imageWidth = (1.143 * screenHeight - 20);
+const leftPadding = 0.128 * imageWidth;
+const topPadding = 0.044 * imageHeight;
 
 
 const pairsOfArray = array => (
@@ -51,7 +51,7 @@ const ColoredRect = ({X, Y, prevX, prevY}) => {
           x:  X,
           y: Y,
           fill: 'red',
-          width: 6
+          width: 4
         }}
       >
         {props => (
@@ -61,13 +61,14 @@ const ColoredRect = ({X, Y, prevX, prevY}) => {
     );
   }
 
-const collisionBoundary = 500
+const collisionBoundary = 300
 
 const detectCollision = (newPositions, goneBack) => {
   const newCoords = newPositions.map((p, i) => ({
     x: testingHumans[i][p].x,
     y: testingHumans[i][p].y
   }))
+  // console.log(newCoords[128], newCoords[118])
   let collisionDetected = false;
   let newIndex = 10000000;
   const combinations = pairsOfArray(newCoords);
@@ -75,9 +76,16 @@ const detectCollision = (newPositions, goneBack) => {
   for (let i = 0; i < comboLength; i++) {
     const {first, second, indexes} = combinations[i];
     if ((Math.abs(first.x - second.x) < collisionBoundary) && (Math.abs(first.y - second.y) < collisionBoundary)) {
+        if (newPositions[indexes[0]] === testingHumans[indexes[0]].length - 1 ||
+          newPositions[indexes[1]] === testingHumans[indexes[1]].length - 1) {
+            continue
+          }
+        // console.log(`collision between ${indexes[0]} and ${indexes[1]}`);
         if (goneBack.includes(indexes[0])) {
+          // console.log(`${indexes[1]} went back`)
           newIndex = indexes[1]
         } else {
+          // console.log(`${indexes[0]} went back`)
           newIndex = indexes[0]
         }
       collisionDetected = true;
@@ -105,7 +113,6 @@ const detectCollisions = humans => {
   }
   return collisionDetected;
 }
-
 const App = () => {
   const [humanPositions, setHumanPosition] = React.useState(Array(testingHumans.length).fill(0))
   const prevPositions = usePrevious(humanPositions) || Array(testingHumans.length).fill(0) ;
@@ -116,36 +123,43 @@ const App = () => {
     setHumanPosition(newPositions);
   }
   const goForward = () => {
-    const newPositions = humanPositions.map(p => p + 1)
+    const newPositions = humanPositions.map((p, i) => Math.min(testingHumans[i].length - 1, p + 1))
     setStepNumber(stepNumber + 1);
-    let times = 0;
     let updatedNewPositions = newPositions;
     const {collisionDetected, goneBack} = detectCollision(updatedNewPositions, []);
     let collision = collisionDetected;
     let humansStayed = goneBack;
-    while (collision && times < 200) {
+    while (collision) {
       updatedNewPositions = newPositions.map((n, i) => humansStayed.includes(i) ? n - 1 : n);
       const {collisionDetected, goneBack} = detectCollision(updatedNewPositions, humansStayed);
       collision = collisionDetected;
       humansStayed = goneBack;
-      times += 1;
     }
     setHumanPosition(updatedNewPositions);
   }
+  // console.log('8', humanPositions[8]);
+  // console.log('9', humanPositions[9]);
+  // console.log('12', humanPositions[12]);
 
   return (
     <div className="App">
       <img src={floorPlan} className="App-logo" alt="logo" />
       <div style={{position: 'absolute', top: 10, left: `calc(50vw - ${imageWidth/2}px)`, width: imageWidth, height: imageHeight}}>
-      <Stage height={0.91 * imageHeight} width={0.6388 * imageWidth} style={{position: 'absolute', top: topPadding, left:leftPadding}}>
+      <Stage height={0.902 * imageHeight} width={0.718 * imageWidth} style={{position: 'absolute', top: topPadding, left:leftPadding}}>
           <Layer>
             {testingHumans.map((h, i) => (
+              // <Text
+              //   text={i}
+              //   fontSize={8}
+              //   x={h[humanPositions[i]].x/65.8}
+              //   y={(46280 - h[humanPositions[i]].y)/65.8}
+              // />
               <ColoredRect
                 key={i}
-                prevX={h[prevPositions[i]].x/71.9}
-                prevY={(46280 - h[prevPositions[i]].y)/72.1}
-                X={h[humanPositions[i]].x/71.9}
-                Y={(46280 - h[humanPositions[i]].y)/72.1}
+                prevX={h[prevPositions[i]].x/65.8}
+                prevY={(46280 - h[prevPositions[i]].y)/65.8}
+                X={h[humanPositions[i]].x/65.8}
+                Y={(46280 - h[humanPositions[i]].y)/65.8}
               />
             ))}
           </Layer>
