4a4b25524508da7a051d14df9c1245ee89787174
Odysseas Zafeiriou <odysseas@wearereasonablepeople.com>
2020-03-02 01:04:44 +0200

    Updates

---
+++ precache-manifest.844f43af00464b7f2d36f87e74d63122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf6be01d6a5a88e09b6b87448ae9dc6",
+    "url": "/evacuation-plan/index.html"
+  },
+  {
+    "revision": "67cec1ef72e66ac8b83e",
+    "url": "/evacuation-plan/static/css/main.599729ce.chunk.css"
+  },
+  {
+    "revision": "fb203078d034975e4216",
+    "url": "/evacuation-plan/static/js/2.35dfb103.chunk.js"
+  },
+  {
+    "revision": "3aed3ae0aff2303b02de41c27714496b",
+    "url": "/evacuation-plan/static/js/2.35dfb103.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67cec1ef72e66ac8b83e",
+    "url": "/evacuation-plan/static/js/main.e0cf844d.chunk.js"
+  },
+  {
+    "revision": "f057ea6631c41da54025",
+    "url": "/evacuation-plan/static/js/runtime-main.712ae75e.js"
+  },
+  {
+    "revision": "11102fb2bd1e7c1b8d942388f5384640",
+    "url": "/evacuation-plan/static/media/floor-plan.11102fb2.jpg"
+  }
+]);
